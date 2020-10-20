2abcde61116ab6a4ec42a94beaa48731d118f5ff
Guillaume <xlgktakx@gmail.com>
2020-10-21 01:19:13 +0200

    Updates

---
+++ precache-manifest.c577f0310d001da9058b00cf1d3e51cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8053e19f1e277dd958c61a0784eae588",
+    "url": "/color-selector/index.html"
+  },
+  {
+    "revision": "0e0651373d6fd92d98de",
+    "url": "/color-selector/static/css/main.47eebea6.chunk.css"
+  },
+  {
+    "revision": "5f167d00b855a644224a",
+    "url": "/color-selector/static/js/2.4f500c46.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/color-selector/static/js/2.4f500c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0651373d6fd92d98de",
+    "url": "/color-selector/static/js/main.e524d561.chunk.js"
+  },
+  {
+    "revision": "7c8b1bc5dd9d29a3853c",
+    "url": "/color-selector/static/js/runtime-main.247e9395.js"
+  }
+]);
