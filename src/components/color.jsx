0e367dd29dcc9583e7f7f83eadeafacc94002ee4
Guillaume <xlgktakx@gmail.com>
2020-10-27 03:19:27 +0100

    add locked

--- src/components/color.jsx
+++ src/components/color.jsx
@@ -29,24 +29,79 @@ class Color extends Component {
     return -1;
   };
 
+  returnCadenaSvg = (textFillColor) => {
+    if (this.props.locked)
+      return (
+        <svg
+          width="24"
+          height="29"
+          viewBox="0 0 24 29"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            y="10.2448"
+            width="24"
+            height="18"
+            rx="4"
+            fill={textFillColor}
+            className="cadenas"
+          />
+          <path
+            d="M6 7.86956C5.99999 2.32609 9.07107 2 12 2C14.9289 2 18 2 18 7.86956C18 9.90159 18 11.621 18 13.0006C18 15.2097 16.2091 17 14 17H10C7.79087 17 6 15.211 6 13.0018C6 11.5926 6 9.85679 6 7.86956Z"
+            stroke={textFillColor}
+            stroke-width="3"
+            className="cadenas"
+          />
+        </svg>
+      );
+    else
+      return (
+        <svg
+          width="24"
+          height="31"
+          viewBox="0 0 24 31"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            y="12.2448"
+            width="24"
+            height="18"
+            rx="4"
+            fill={textFillColor}
+            className="cadenas"
+          />
+          <path
+            d="M6 6.69565C5.99999 2.26087 9.07107 2 12 2C14.9289 2 18 2 18 6.69565C18 11.3913 18 14 18 14"
+            stroke={textFillColor}
+            stroke-width="3"
+            className="cadenas"
+          />
+        </svg>
+      );
+  };
+
   render() {
+    const textFillColor = this.getOpositeColor(this.props.rgb);
+    const cadenaSVG = this.returnCadenaSvg(textFillColor);
     return (
       <div className="colorContainer">
         <div
-          className={this.props.class}
+          className={this.props.locked ? "myColor myColorActivated" : this.props.class}
           style={{
             backgroundColor: this.props.hexa,
             transitionDelay: this.props.transitionDelay,
-            color: this.getOpositeColor(this.props.rgb),
+            color: textFillColor,
           }}
         >
           <p className="text" onClick={() => this.copyText(this.props.hexa)}>
             {this.props.hexa}
           </p>
-          <br />
           <p className="text" onClick={() => this.copyText(this.props.rgb)}>
             {this.props.rgb}
           </p>
+          <span onClick={() => this.props.triggerLocked(this.props.id)}>{cadenaSVG}</span>
         </div>
       </div>
     );
--- src/components/colors.jsx
+++ src/components/colors.jsx
@@ -7,14 +7,70 @@ import "./colors.css";
 class Colors extends Component {
   state = {
     colors: [
-      { id: 1, hexa: "#000000", rgb: "rgb(0,0,0)", transitionDelay: "0s", textColor: "#FFFFFF" },
-      { id: 2, hexa: "#000000", rgb: "rgb(0,0,0)", transitionDelay: "0.1s", textColor: "#ffffff" },
-      { id: 3, hexa: "#000000", rgb: "rgb(0,0,0)", transitionDelay: "0.2s", textColor: "#ffffff" },
-      { id: 4, hexa: "#000000", rgb: "rgb(0,0,0)", transitionDelay: "0.3s", textColor: "#ffffff" },
-      { id: 5, hexa: "#000000", rgb: "rgb(0,0,0)", transitionDelay: "0.3s", textColor: "#ffffff" },
-      { id: 6, hexa: "#000000", rgb: "rgb(0,0,0)", transitionDelay: "0.2s", textColor: "#ffffff" },
-      { id: 7, hexa: "#000000", rgb: "rgb(0,0,0)", transitionDelay: "0.1s", textColor: "#ffffff" },
-      { id: 8, hexa: "#000000", rgb: "rgb(0,0,0)", transitionDelay: "0s", textColor: "#ffffff" },
+      {
+        id: 1,
+        hexa: "#000000",
+        rgb: "rgb(0,0,0)",
+        transitionDelay: "0s",
+
+        locked: false,
+      },
+      {
+        id: 2,
+        hexa: "#000000",
+        rgb: "rgb(0,0,0)",
+        transitionDelay: "0.1s",
+
+        locked: false,
+      },
+      {
+        id: 3,
+        hexa: "#000000",
+        rgb: "rgb(0,0,0)",
+        transitionDelay: "0.2s",
+
+        locked: false,
+      },
+      {
+        id: 4,
+        hexa: "#000000",
+        rgb: "rgb(0,0,0)",
+        transitionDelay: "0.3s",
+
+        locked: false,
+      },
+      {
+        id: 5,
+        hexa: "#000000",
+        rgb: "rgb(0,0,0)",
+        transitionDelay: "0.3s",
+
+        locked: false,
+      },
+      {
+        id: 6,
+        hexa: "#000000",
+        rgb: "rgb(0,0,0)",
+        transitionDelay: "0.2s",
+
+        locked: false,
+      },
+      {
+        id: 7,
+        hexa: "#000000",
+        rgb: "rgb(0,0,0)",
+        transitionDelay: "0.1s",
+
+        locked: false,
+      },
+      {
+        id: 8,
+        hexa: "#000000",
+        rgb: "rgb(0,0,0)",
+        transitionDelay: "0s",
+
+        locked: false,
+      },
     ],
     colorClass: "myColor",
     copiedClass: "background backgroundDesactivated",
@@ -23,10 +79,12 @@ class Colors extends Component {
 
   randomizeColor = () => {
     const colors = this.state.colors.map((c) => {
-      const color = ~~(Math.random() * 0xffffff);
-      c.hexa = "#" + color.toString(16).padStart(6, "0").toUpperCase();
-      c.rgb = "RGB(" + (color >> 16) + "," + ((color >> 8) & 0xff) + "," + (color & 0xff) + ")";
-      return c;
+      if (!c.locked) {
+        const color = ~~(Math.random() * 0xffffff);
+        c.hexa = "#" + color.toString(16).padStart(6, "0").toUpperCase();
+        c.rgb = "RGB(" + (color >> 16) + "," + ((color >> 8) & 0xff) + "," + (color & 0xff) + ")";
+        return c;
+      } else return c;
     });
     this.setState({ colors });
   };
@@ -50,6 +108,12 @@ class Colors extends Component {
     }, 500);
   };
 
+  triggerLocked = (e) => {
+    let colors = [...this.state.colors];
+    colors[e - 1].locked = !this.state.colors[e - 1].locked;
+    this.setState({ colors });
+  };
+
   render() {
     return (
       <div className="appContainer">
@@ -59,11 +123,13 @@ class Colors extends Component {
           {this.state.colors.map((color) => (
             <Color
               key={color.id}
+              id={color.id}
               hexa={color.hexa}
               rgb={color.rgb}
               class={this.state.colorClass}
               transitionDelay={color.transitionDelay}
-              textColor={color.textColor}
+              locked={color.locked}
+              triggerLocked={this.triggerLocked}
               triggerCopied={this.triggerCopied}
             />
           ))}
